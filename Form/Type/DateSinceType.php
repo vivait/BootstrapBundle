600d4220dbc4ee2d0826b74c628a23f9cb361ac8
Lewis Wright <lewis@subora.com>
2014-01-31 16:51:04 +0000

    Moved inline javascript from base.html.twig to an external file. Nearly finished the date since widget (JS is still inline!)

--- Form/Type/DateSinceType.php
+++ Form/Type/DateSinceType.php
@@ -4,6 +4,9 @@ namespace Vivait\BootstrapBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
 
 class DateSinceType extends AbstractType
 {
@@ -12,22 +15,23 @@ class DateSinceType extends AbstractType
 	 */
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-//			$builder->addViewTransformer(new DateTimeToLocalizedStringTransformer(
-//				$options['model_timezone'],
-//				$options['view_timezone'],
-//				$dateFormat,
-//				$timeFormat,
-//				$calendar,
-//				$pattern
-//			));
 
-		$builder
-			->add('since', 'text', array());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function finishView(FormView $view, FormInterface $form, array $options)
+	{
+		if ('single_text' === $options['widget']) {
+			$view->vars['date_moment_pattern'] = strtoupper($options['format']);
+		}
 	}
 
 	public function setDefaultOptions(OptionsResolverInterface $resolver)
 	{
 		$resolver->setDefaults(array(
+			'horizontal_input_wrapper_class' => ''
 		));
 	}
 
