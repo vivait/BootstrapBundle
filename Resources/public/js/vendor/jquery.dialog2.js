c8df2a28c15bfa673d2f392ac1cbe865a4ad6ac1
Lewis Wright <lewis@subora.com>
2014-02-07 10:59:54 +0000

    Fixed 'dialogError' not existing with jQuery Dialog2.

--- Resources/public/js/vendor/jquery.dialog2.js
+++ Resources/public/js/vendor/jquery.dialog2.js
@@ -259,7 +259,7 @@
                     beforeSubmit: dialog.__ajaxStartTrigger,
                     beforeSend: dialog.__ajaxBeforeSend,
                     error: function() {
-                        throw dialogError("Form submit failed: " + $.makeArray(arguments));
+                        throw Error("Form submit failed: " + $.makeArray(arguments));
                     }
                 };
 
