0125d41dc9b8f4df3a9afa682575742725a1a7b2
Lewis Wright <lewis@vivait.co.uk>
2014-04-20 17:04:07 +0100

    Added a helper function to dispatch entity modified events and arrays of events.

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,7 +2,9 @@
 
 namespace Vivait\BootstrapBundle\Controller;
 
+use Doctrine\ORM\UnitOfWork;
 use Symfony\Component\HttpFoundation\Request;
+use Vivait\Common\Event\EntityEvent;
 
 class Controller extends \Symfony\Bundle\FrameworkBundle\Controller\Controller
 {
@@ -19,16 +21,48 @@ class Controller extends \Symfony\Bundle\FrameworkBundle\Controller\Controller
 			$parent = $this->generateUrl('viva_app_homepage');
 		}
 
-		return $this->redirectTo($parent);
+		return $this->redirectTo($request, $parent);
 	}
 
-	public function redirectTo($to) {
-		if ($to->headers->get('X-REQUESTED-WITH') == 'XMLHttpRequest') {
+	public function redirectTo(Request $request, $to) {
+		if ($request->headers->get('X-REQUESTED-WITH') == 'XMLHttpRequest') {
 			return $this->render('VivaitBootstrapBundle:Default:redirect.html.twig', array(
 				'redirect' => $to
 			));
 		}
 
 		return $this->redirect($to);
+	}
+
+	public function dispatchEntityEvent($events) {
+		$em         = $this->getDoctrine()->getManager();
+		$dispatcher = $this->get('event_dispatcher');
+
+		/* @var $uow \Doctrine\ORM\UnitOfWork */
+		$uow = $em->getUnitOfWork();
+		$uow->computeChangeSets();
+
+		if (is_array($events)) {
+			// Dispatch each event
+			array_walk($events, function(EntityEvent $event) use ($uow, $dispatcher) {
+				$this->dispatchEntityEventRunner($event, $uow, $dispatcher);
+			});
+		}
+		else {
+			$this->dispatchEntityEventRunner($events, $uow, $dispatcher);
+		}
+	}
+
+	protected function dispatchEntityEventRunner(EntityEvent $event, UnitOfWork $uow, $dispatcher = null) {
+		if (!$dispatcher) {
+			$dispatcher = $this->get('event_dispatcher');
+		}
+
+		if ($uow->isScheduledForUpdate($event->getEntity())) {
+			$dispatcher->dispatch(EntityEvent::EVENT_ENTITY_MODIFIED, $event);
+		}
+		else if ($uow->isScheduledForInsert($event->getEntity())) {
+			$dispatcher->dispatch(EntityEvent::EVENT_ENTITY_CREATED, $event);
+		}
 	}
 }
