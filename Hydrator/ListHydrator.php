233945b8becab3645749559ef9331f23aba35742
Lewis Wright <lewis@vivait.co.uk>
2014-11-06 11:21:23 +0000

    Fixes to list hydrator

--- Hydrator/ListHydrator.php
+++ Hydrator/ListHydrator.php
@@ -12,30 +12,30 @@ class ListHydrator extends AbstractHydrator
         $result = $cache = array();
 
         foreach ($this->_stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
-            $this->hydrateRowData($row, $cache, $result);
+            $this->hydrateRowData($row, $result);
         }
 
         return $result;
     }
 
-    protected function hydrateRowData(array $row, array &$cache, array &$result)
+    protected function hydrateRowData(array $data, array &$result)
     {
-        if (count($row) == 0) {
+        if (count($data) == 0) {
             return false;
         }
 
-        $keys = array_keys($row);
+        $keys = array_keys($data);
 
         // Assume first column is id field
-        $id = $row[$keys[0]];
+        $id = $data[$keys[0]];
 
-        if (count($row) == 2) {
+        if (count($data) == 2) {
             // If only one more field assume that this is the value field
-            $value = $row[$keys[1]];
+            $value = $data[$keys[1]];
         } else {
             // Remove ID field and add remaining fields as value array
-            array_shift($row);
-            $value = $row;
+            array_shift($data);
+            $value = $data;
         }
 
         $result[$id] = $value;
