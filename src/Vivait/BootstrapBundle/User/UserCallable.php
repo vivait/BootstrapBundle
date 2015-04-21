3e03b809d70267c6a1ab62ed255efa13c63602e6
Robin Cawser <robin@vivait.co.uk>
2015-04-21 09:26:42 +0100

    Change getCurrentUser to return null if no token set, instead of PHP Fatal.

--- src/Vivait/BootstrapBundle/User/UserCallable.php
+++ src/Vivait/BootstrapBundle/User/UserCallable.php
@@ -4,25 +4,35 @@ namespace Vivait\BootstrapBundle\User;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\SecurityContext;
 use Vivait\Common\User\UserCallableInterface;
 
 class UserCallable implements UserCallableInterface {
-	/* @var ContainerInterface */
-	protected $container;
+    /* @var ContainerInterface */
+    protected $container;
 
-	/**
-	 * @param ContainerInterface $container
-	 */
-	public function __construct( ContainerInterface $container ) {
-		$this->container = $container;
-	}
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct( ContainerInterface $container ) {
+        $this->container = $container;
+    }
 
-	/**
-	 * @{inheritdoc}
-	 */
-	public function getCurrentUser() {
-		/* @var $token TokenInterface */
-		$token = $this->container->get('security.context')->getToken();
-		return $token->getUser() ?: null;
-	}
+    /**
+     * @{inheritdoc}
+     */
+    public function getCurrentUser() {
+
+        /** @var SecurityContext $context */
+        $context = $this->container->get('security.context');
+
+        /** @var $token TokenInterface */
+        $token = $context->getToken();
+
+        if($token){
+            return $token->getUser() ?: null;
+        } else {
+            return null;
+        }
+    }
 }
