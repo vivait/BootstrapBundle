da243047c44e71235bf6ef0189aab64857b15851
Kiel Goodman <kiel@vivait.co.uk>
2015-05-28 09:46:06 +0100

    getEntityTypeLabel fix

--- EventListener/EntityFlashBagListener.php
+++ EventListener/EntityFlashBagListener.php
@@ -6,61 +6,63 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Translation\TranslatorInterface;
 use Vivait\Common\Event\EntityEvent;
 
-class EntityFlashBagListener {
-	/**
-	 * @var Session
-	 */
-	private $session;
+class EntityFlashBagListener
+{
+    /**
+     * @var Session
+     */
+    private $session;
 
-	/**
-	 * @var TranslatorInterface
-	 */
-	private $translator;
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
 
-	/**
-	 * @param Session $session
-	 * @param TranslatorInterface $translator
-	 */
-	public function __construct(Session $session, TranslatorInterface $translator)
-	{
-		$this->session    = $session;
-		$this->translator = $translator;
-	}
+    /**
+     * @param Session $session
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(Session $session, TranslatorInterface $translator)
+    {
+        $this->session = $session;
+        $this->translator = $translator;
+    }
 
-	/**
-	 * @param \Vivait\Common\Event\EntityEvent $event
-	 */
-	public function onEntityModifiedEvent(EntityEvent $event)
-	{
-		$this->getFlashBag()->add('success', $this->translator->trans('The %entity_name% has been modified successfully!', [
-			'%entity_name%' => $event::getEntityName()
-		]));
-	}
+    /**
+     * @param \Vivait\Common\Event\EntityEvent $event
+     */
+    public function onEntityModifiedEvent(EntityEvent $event)
+    {
+        $this->getFlashBag()->add('success', $this->translator->trans('The %entity_name% has been modified successfully!', [
+            '%entity_name%' => $event::getEntityTypeLabel()
+        ]));
+    }
 
-	/**
-	 * @param \Vivait\Common\Event\EntityEvent $event
-	 */
-	public function onEntityCreatedEvent(EntityEvent $event)
-	{
-		$this->getFlashBag()->add('success', $this->translator->trans('The %entity_name% has been created successfully!', [
-			'%entity_name%' => $event::getEntityName()
-		]));
-	}
+    /**
+     * @param \Vivait\Common\Event\EntityEvent $event
+     */
+    public function onEntityCreatedEvent(EntityEvent $event)
+    {
+        $this->getFlashBag()->add('success', $this->translator->trans('The %entity_name% has been created successfully!', [
+            '%entity_name%' => $event::getEntityTypeLabel()
+        ]));
+    }
 
-	/**
-	 * @param \Vivait\Common\Event\EntityEvent $event
-	 */
-	public function onEntityDeletedEvent(EntityEvent $event)
-	{
-		$this->getFlashBag()->add('success', $this->translator->trans('The %entity_name% has been deleted', [
-			'%entity_name%' => $event::getEntityName()
-		]));
-	}
+    /**
+     * @param \Vivait\Common\Event\EntityEvent $event
+     */
+    public function onEntityDeletedEvent(EntityEvent $event)
+    {
+        $this->getFlashBag()->add('success', $this->translator->trans('The %entity_name% has been deleted', [
+            '%entity_name%' => $event::getEntityTypeLabel()
+        ]));
+    }
 
-	/**
-	 * @return \Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface
-	 */
-	private function getFlashBag() {
-		return $this->session->getFlashBag();
-	}
+    /**
+     * @return \Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface
+     */
+    private function getFlashBag()
+    {
+        return $this->session->getFlashBag();
+    }
 }
